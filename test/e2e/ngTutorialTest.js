b469860e9836259ba6cb13c11be7f8513594b8fe
daishi <daishi@axlight.com>
2013-05-01 09:15:23 +0900

    ng tutorial test 02 uses js on github

--- test/e2e/ngTutorialTest.js
+++ test/e2e/ngTutorialTest.js
@@ -9,9 +9,9 @@ describe('angular tutorial e2e test for prerenderer', function() {
     sleep(1);
     expect(element('ul li:nth-child(1)').text()).toContain('Nexus S');
     expect(element('ul li:nth-child(1)').text()).toContain('Fast just got faster with Nexus S.');
-    expect(element('ul li:nth-child(2)').text()).toContain('Motorola XOOM^(TM) with Wi-Fi');
+    expect(element('ul li:nth-child(2)').text()).toContain('Motorola XOOM');
     expect(element('ul li:nth-child(2)').text()).toContain('The Next, Next Generation tablet.');
-    expect(element('ul li:nth-child(3)').text()).toContain('MOTOROLA XOOM^(TM)');
+    expect(element('ul li:nth-child(3)').text()).toContain('MOTOROLA XOOM');
     expect(element('ul li:nth-child(3)').text()).toContain('The Next, Next Generation tablet.');
   });
 
@@ -19,6 +19,10 @@ describe('angular tutorial e2e test for prerenderer', function() {
     browser().navigateTo('/PRERENDER-ngtutorial02.html');
     expect(element('ul li:nth-child(1)').text()).toContain('Nexus S');
     expect(element('ul li:nth-child(1)').text()).toContain('Fast just got faster with Nexus S.');
+    expect(element('ul li:nth-child(2)').text()).toContain('Motorola XOOM');
+    expect(element('ul li:nth-child(2)').text()).toContain('The Next, Next Generation tablet.');
+    expect(element('ul li:nth-child(3)').text()).toContain('MOTOROLA XOOM');
+    expect(element('ul li:nth-child(3)').text()).toContain('The Next, Next Generation tablet.');
   });
 
 
