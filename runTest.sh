69683dd28c25f5fdcb338e9af3212045d8012e24
daishi <daishi@axlight.com>
2014-05-19 22:36:33 +0900

    runTest.sh returns status

--- runTest.sh
+++ runTest.sh
@@ -6,6 +6,10 @@ node ./test/server/app.js &
 PID=$!
 
 ./node_modules/karma/bin/karma start ./test/e2e.conf
+RES1=$?
 ./node_modules/karma/bin/karma start ./test/e2e-2.conf
+RES2=$?
 
 kill $PID
+
+exit $RES1 || $RES2
