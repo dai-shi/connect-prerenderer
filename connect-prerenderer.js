78c351de534a1f7cb6d437ad6b7552a42b4e5d27
Quentin Raynaud <quentin@qraynaud.eu>
2015-05-23 15:38:32 +0200

    use a subprocess for isolation

--- connect-prerenderer.js
+++ connect-prerenderer.js
@@ -27,9 +27,7 @@
 /* jshint evil: true */
 
 var http = require('http');
-var urlLib = require('url');
-var request = require('request');
-var jsdom = require('jsdom');
+var spawn = require('child_process').spawn;
 
 var targetGeneratorMap = {
 
@@ -83,72 +81,53 @@ function getTargetURL(req, options) {
   return targetGenerator(req.url, options, req);
 }
 
-function filterHeaders(headers) {
-  var newHeaders = {};
-  for (var key in headers) {
-    if (key === 'host' || key === 'cookie' ||
-      (key.lastIndexOf('accept', 0) === 0 && key !== 'accept-encoding')) {
-      newHeaders[key] = headers[key];
-    }
-  }
-  return newHeaders;
-}
-
 function renderURL(url, headers, options, callback) {
+  var renderer = spawn(process.argv[0], [__dirname + '/urlRenderer.js', JSON.stringify([url, headers, options])], {
+    stdio: ['ignore', 'pipe', 'pipe']
+  });
+
+  renderer.stderr.pipe(process.stderr);
+
+  var allData = new Buffer('');
+  renderer.stdout.on('data', function(data) {
+    allData = Buffer.concat([allData, data]);
+  });
+
   var timeout = (options && options.timeout ? options.timeout : 5000);
-  var cookieDomain = options && options.cookieDomain;
-  request({
-    uri: urlLib.parse(url),
-    headers: filterHeaders(headers)
-  }, function(err, res, body) {
-    if (err) {
-      callback(err);
-      return;
+  var timer = null;
+  function done(err) {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
     }
-    if (res.statusCode != 200) {
-      callback(res.statusCode);
-      return;
-    }
-    var document;
-    var timer = null;
-    var done = function() {
-      if (timer) {
-        clearTimeout(timer);
-        timer = null;
-        var content;
-        try {
-          document.body.setAttribute('data-prerendered', 'true');
-          content = jsdom.serializeDocument(document);
-        } catch (err) {
-          callback(err);
-          return;
-        }
-        callback(null, content, res.headers);
-      }
-    };
-    timer = setTimeout(done, timeout);
+    renderer.kill('SIGKILL');
+
+    var args
     try {
-      document = jsdom.jsdom(body, {
-        url: url,
-        cookie: headers.cookie,
-        cookieDomain: cookieDomain,
-        features: {
-          FetchExternalResources: ['script'],
-          ProcessExternalResources: ['script']
-        }
-      });
-      if (options && options.attachConsole) {
-        document.parentWindow.console = console;
-      }
-      document.onprerendered = done;
-    } catch (err) {
-      if (timer) {
-        clearTimeout(timer);
-        timer = null;
-      }
-      callback(err);
-      return;
+      args = JSON.parse(allData);
     }
+    catch(e) {
+      args = [e];
+    }
+
+    if (args[0] && !(args[0] instanceof Error))
+      args[0] = new Error(args[0]);
+    else if (err)
+      args[0] = err;
+
+    callback.apply(null, args);
+  }
+
+  timer = setTimeout(function() {
+    console.log('Timouted after ' + timeout + ' ms');
+    done();
+  }, timeout);
+
+  renderer.stdout.on('end', done);
+
+  renderer.stdout.on('error', function(error) {
+    renderer.kill('SIGKILL');
+    callback(err);
   });
 }
 
